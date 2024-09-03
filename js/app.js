e7e7c39a21b4e7449073ded346153affdebc4091
Alejandria98 <75117884+Alejandria98@users.noreply.github.com>
2024-09-03 16:29:39 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    console.log("helllo");
+
+  
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true,
+        loop: true,
+        margin:0,
+        responsive : {
+        291 : {
+        items:1
+        },
+        751 : {
+        items:1
+        },
+        1011 : {
+        items:1
+        }
+        }
+        });
+
+
+
+
+
+
+
+
+
+
+        
+
+});
